4dfca9df2f62ca0ec05f64f9846ff8be1c745818
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-05-20 18:48:31 +0200

    Create mock data to test coverage

--- PMS.Backend.Test/UnitTest1.cs
+++ PMS.Backend.Test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using PMS.Backend.Features.Registration.Controllers;
+
 namespace PMS.Backend.Test;
 
 public class UnitTest1
@@ -5,5 +7,10 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        // Arrange
+        // Act
+        // Assert
+        var controller = new RegistrationController();
+        Assert.NotNull(controller);
     }
 }
