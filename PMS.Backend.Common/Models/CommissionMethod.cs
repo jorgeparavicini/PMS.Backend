9630831c3c8e5ab6899072c2b829ce323eb8538b
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-05-21 17:47:51 +0200

    Add Agency Entity

--- PMS.Backend.Common/Models/CommissionMethod.cs
+++ PMS.Backend.Common/Models/CommissionMethod.cs
@@ -2,5 +2,6 @@
 
 public enum CommissionMethod
 {
-    
+    DeductedByAgency,
+    DeductedByProvider,
 }
--- PMS.Backend.Core/Entities/Agency.cs
+++ PMS.Backend.Core/Entities/Agency.cs
@@ -1,6 +1,40 @@
-﻿namespace PMS.Backend.Core.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using PMS.Backend.Common.Models;
+
+namespace PMS.Backend.Core.Entities;
 
 public class Agency
 {
+    public int Id { get; set; }
     
+    [MaxLength(255)]
+    public string LegalName { get; set; }
+    
+    public decimal DefaultCommissionRate { get; set; }
+    
+    public decimal DefaultCommissionOnExtras { get; set; }
+    
+    public CommissionMethod CommissionMethod { get; set; }
+    
+    [MaxLength(255)]
+    public string? EmergencyPhone { get; set; }
+    
+    [MaxLength(255)]
+    public string? EmergencyEmail { get; set; }
+
+    public Agency(
+        string legalName,
+        decimal defaultCommissionRate,
+        decimal defaultCommissionOnExtras,
+        CommissionMethod commissionMethod,
+        string? emergencyPhone,
+        string? emergencyEmail)
+    {
+        LegalName = legalName;
+        DefaultCommissionRate = defaultCommissionRate;
+        DefaultCommissionOnExtras = defaultCommissionOnExtras;
+        CommissionMethod = commissionMethod;
+        EmergencyPhone = emergencyPhone;
+        EmergencyEmail = emergencyEmail;
+    }
 }
