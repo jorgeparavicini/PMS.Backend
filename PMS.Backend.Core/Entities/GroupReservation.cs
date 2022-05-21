c16dbfc2bcd672188bbec6310f69ef5be71b0337
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-05-21 19:14:24 +0200

    Add reservation details

--- PMS.Backend.Core/Entities/GroupReservation.cs
+++ PMS.Backend.Core/Entities/GroupReservation.cs
@@ -7,32 +7,37 @@ public class GroupReservation
     #region Properties
 
     public int Id { get; set; }
-    
+
     [MaxLength(255)]
     public string? Reference { get; set; }
-    
+
     public DateTime ReservationDate { get; set; }
-    
+
     public bool IsQuote { get; set; }
 
     #endregion
-    
+
     #region Relations
 
     public int AgencyContactId { get; set; }
     public AgencyContact AgencyContact { get; set; } = null!;
-    
+
     public IList<Reservation> Reservations { get; } = new List<Reservation>();
 
     #endregion
 
-    public GroupReservation(string? reference, DateTime reservationDate, bool isQuote)
+    public GroupReservation(
+        string? reference,
+        DateTime reservationDate,
+        bool isQuote,
+        int agencyContactId)
     {
         Reference = reference;
         ReservationDate = reservationDate;
         IsQuote = isQuote;
+        AgencyContactId = agencyContactId;
     }
-    
+
     // TODO: Branches
     // TODO: Quote ID: not sure what is meant by that
     // TODO: Are BookingType, Channel, Origination Enums?
---
+++ PMS.Backend.Core/Entities/ReservationDetails.cs
@@ -0,0 +1,43 @@
+﻿namespace PMS.Backend.Core.Entities;
+
+public class ReservationDetails
+{
+    #region Properties
+
+    public int Id { get; set; }
+    
+    public DateTime ReservationDate { get; set; }
+    
+    public DateTime CheckIn { get; set; }
+    
+    public DateTime CheckOut { get; set; }
+    
+    public DateTime FolioClosedOn { get; set; }
+
+    #endregion
+
+    #region Relations
+
+    public int ReservationId { get; set; }
+    public Reservation Reservation { get; set; } = null!;
+
+    #endregion
+
+    public ReservationDetails(
+        DateTime reservationDate,
+        DateTime checkIn,
+        DateTime checkOut,
+        DateTime folioClosedOn,
+        int reservationId)
+    {
+        ReservationDate = reservationDate;
+        CheckIn = checkIn;
+        CheckOut = checkOut;
+        FolioClosedOn = folioClosedOn;
+        ReservationId = reservationId;
+    }
+
+    // TODO: Reservations package, pax
+    // TODO: What is package?
+    // TODO: Complimentary - enum?
+}
