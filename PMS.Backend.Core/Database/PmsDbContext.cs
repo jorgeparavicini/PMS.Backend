c46294a3ebcd6ac8505424003038bdd6dae76f21
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-05-22 17:40:38 +0200

    Add DbSets to context

--- PMS.Backend.Core/Database/PmsDbContext.cs
+++ PMS.Backend.Core/Database/PmsDbContext.cs
@@ -6,7 +6,14 @@ namespace PMS.Backend.Core.Database
     public class PmsDbContext : DbContext
     {
         public DbSet<Agency> Agencies => Set<Agency>();
+        
         public DbSet<AgencyContact> AgencyContacts => Set<AgencyContact>();
+
+        public DbSet<GroupReservation> GroupReservations => Set<GroupReservation>();
+
+        public DbSet<Reservation> Reservations => Set<Reservation>();
+
+        public DbSet<ReservationDetail> ReservationDetails => Set<ReservationDetail>();
 
         public PmsDbContext(DbContextOptions<PmsDbContext> options) : base(options)
         {
