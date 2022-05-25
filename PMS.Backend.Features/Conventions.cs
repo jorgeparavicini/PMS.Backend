7a27d521583d1dbe64563c9723cbf3d0dce50964
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-05-25 18:42:43 +0200

    Update conventions to include most common Requests

---
+++ PMS.Backend.Features/Conventions.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+// ReSharper disable UnusedParameter.Global
+
+namespace PMS.Backend.Features;
+
+public static class Conventions
+{
+    #region GET
+
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType]
+    [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+    public static void Get(
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+        [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+        object id)
+    {
+    }
+
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+    public static void Find(
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+        [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+        object id)
+    {
+    }
+
+    #endregion
+
+    #region POST
+
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesDefaultResponseType]
+    [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+    public static void Post(
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+        [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+        object model)
+    {
+    }
+
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesDefaultResponseType]
+    [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+    public static void Create(
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+        [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+        object model)
+    {
+    }
+
+    #endregion
+
+    #region PUT
+
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesDefaultResponseType]
+    [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+    public static void Put(
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+        [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+        object id,
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+        [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+        object model)
+    {
+    }
+
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesDefaultResponseType]
+    [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+    public static void Edit(
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+        [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+        object id,
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+        [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+        object model)
+    {
+    }
+
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesDefaultResponseType]
+    [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+    public static void Update(
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+        [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+        object id,
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+        [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+        object model)
+    {
+    }
+
+    #endregion
+
+    #region DELETE
+
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesDefaultResponseType]
+    [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+    public static void Delete(
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
+        [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+        object id)
+    {
+    }
+
+    #endregion
+}
