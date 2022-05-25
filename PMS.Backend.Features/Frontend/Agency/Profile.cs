3b291283b766b0df801b08a0c9b0483a58ff08a7
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-05-25 18:44:55 +0200

    Create Agency mapping profile

--- PMS.Backend.Features/Frontend/Agency/Profile.cs
+++ PMS.Backend.Features/Frontend/Agency/Profile.cs
@@ -1,4 +1,5 @@
 ﻿using PMS.Backend.Core.Entities;
+using PMS.Backend.Features.Frontend.Agency.Models.Input;
 using PMS.Backend.Features.Frontend.Agency.Models.Output;
 
 namespace PMS.Backend.Features.Frontend.Agency;
@@ -8,6 +9,26 @@ public class Profile : AutoMapper.Profile
     public Profile()
     {
         CreateMap<Core.Entities.Agency, AgencyDTO>();
+        CreateMap<AgencyInputDTO, Core.Entities.Agency>()
+            .ConstructUsing(x => 
+                new Core.Entities.Agency(
+                    x.LegalName,
+                    x.CommissionMethod,
+                    x.DefaultCommissionRate,
+                    x.DefaultCommissionOnExtras,
+                    x.EmergencyPhone,
+                    x.EmergencyEmail));
         CreateMap<AgencyContact, AgencyContactDTO>();
+        CreateMap<AgencyContactInputDTO, AgencyContact>()
+            .ConstructUsing(x =>
+                new AgencyContact(
+                    x.ContactName,
+                    x.IsFrequentVendor,
+                    0,
+                    x.City,
+                    x.ZipCode,
+                    x.Address,
+                    x.Phone,
+                    x.Email));
     }
 }
