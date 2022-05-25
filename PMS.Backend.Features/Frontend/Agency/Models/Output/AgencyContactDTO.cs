03375bb0eba327103271796a30a233d6cd2d9cac
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-05-25 13:45:56 +0200

    Create Agency DTOs

---
+++ PMS.Backend.Features/Frontend/Agency/Models/Output/AgencyContactDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace PMS.Backend.Features.Frontend.Agency.Models.Output;
+
+public record AgencyContactDTO(
+    int Id,
+    string ContactName,
+    string? Email,
+    string? Phone,
+    string? Address,
+    string? City,
+    string? ZipCode,
+    bool IsFrequentVendor)
+{
+    public int Id { get; set; } = Id;
+    public string ContactName { get; set; } = ContactName;
+    public string? Email { get; set; } = Email;
+    public string? Phone { get; set; } = Phone;
+    public string? Address { get; set; } = Address;
+    public string? City { get; set; } = City;
+    public string? ZipCode { get; set; } = ZipCode;
+    public bool IsFrequentVendor { get; set; } = IsFrequentVendor;
+}
---
+++ PMS.Backend.Features/Frontend/Agency/Models/Output/AgencyDTO.cs
@@ -0,0 +1,23 @@
+﻿using PMS.Backend.Common.Models;
+
+namespace PMS.Backend.Features.Frontend.Agency.Models.Output;
+
+public record AgencyDTO(
+    int Id,
+    string LegalName,
+    decimal? DefaultCommissionRate,
+    decimal? DefaultCommissionOnExtras,
+    CommissionMethod CommissionMethod,
+    string? EmergencyPhone,
+    string? EmergencyEmail,
+    IReadOnlyList<AgencyContactDTO> AgencyContacts)
+{
+    public int Id = Id;
+    public string LegalName = LegalName;
+    public decimal? DefaultCommissionRate = DefaultCommissionRate;
+    public decimal? DefaultCommissionOnExtras = DefaultCommissionOnExtras;
+    public CommissionMethod CommissionMethod = CommissionMethod;
+    public string? EmergencyPhone = EmergencyPhone;
+    public string? EmergencyEmail = EmergencyEmail;
+    public IReadOnlyList<AgencyContactDTO> AgencyContacts = AgencyContacts;
+}
