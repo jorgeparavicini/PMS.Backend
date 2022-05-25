333073dcd36625a51bd620826e921188d5ffc734
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-05-25 18:46:02 +0200

    Update feature registrar

---
+++ PMS.Backend.Features/Frontend/Reservation/Registrar.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PMS.Backend.Features.Frontend.Reservation.Controllers;
+using PMS.Backend.Features.Frontend.Reservation.Services;
+using PMS.Backend.Features.Frontend.Reservation.Services.Contracts;
+
+namespace PMS.Backend.Features.Frontend.Reservation;
+
+public static class Registrar
+{
+    public static void AddReservationAPI(this IServiceCollection services)
+    {
+        // Controllers
+        services.AddScoped<ReservationController>();
+        
+        // Services
+        services.AddScoped<IReservationService, ReservationService>();
+    }
+}
