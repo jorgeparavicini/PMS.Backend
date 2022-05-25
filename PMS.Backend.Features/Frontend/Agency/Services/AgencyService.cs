1832c187fa8d50875754fee8b3965352a38a0c77
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-05-25 18:45:35 +0200

    Add create agency method to service

--- PMS.Backend.Features/Frontend/Agency/Services/AgencyService.cs
+++ PMS.Backend.Features/Frontend/Agency/Services/AgencyService.cs
@@ -1,7 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using PMS.Backend.Core.Database;
-using PMS.Backend.Features.Frontend.Agency.Models;
+using PMS.Backend.Features.Frontend.Agency.Models.Input;
 using PMS.Backend.Features.Frontend.Agency.Models.Output;
 using PMS.Backend.Features.Frontend.Agency.Services.Contracts;
 
@@ -11,11 +12,13 @@ public class AgencyService : IAgencyService
 {
     private readonly PmsDbContext _context;
     private readonly IMapper _mapper;
+    private readonly ILogger _logger;
 
-    public AgencyService(PmsDbContext context, IMapper mapper)
+    public AgencyService(PmsDbContext context, IMapper mapper, ILogger<AgencyService> logger)
     {
         _context = context;
         _mapper = mapper;
+        _logger = logger;
     }
 
     public async Task<IEnumerable<AgencyDTO>> GetAllAgenciesAsync()
@@ -28,5 +31,21 @@ public class AgencyService : IAgencyService
     {
         var agency = await _context.Agencies.FindAsync(id);
         return _mapper.Map<AgencyDTO?>(agency);
+    }
+
+    public async Task<int?> CreateAgencyAsync(AgencyInputDTO agency)
+    {
+        var entity = _mapper.Map<Core.Entities.Agency>(agency);
+        await _context.Agencies.AddAsync(entity);
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateException e)
+        {
+            _logger.LogError("Could not save new agency to db. Error: {Message}", e.Message);
+        }
+        
+        return entity.Id > 0 ? entity.Id : null;
     }
 }
--- PMS.Backend.Features/Frontend/Agency/Services/Contracts/IAgencyService.cs
+++ PMS.Backend.Features/Frontend/Agency/Services/Contracts/IAgencyService.cs
@@ -1,4 +1,4 @@
-﻿using PMS.Backend.Features.Frontend.Agency.Models;
+﻿using PMS.Backend.Features.Frontend.Agency.Models.Input;
 using PMS.Backend.Features.Frontend.Agency.Models.Output;
 
 namespace PMS.Backend.Features.Frontend.Agency.Services.Contracts;
@@ -8,4 +8,6 @@ public interface IAgencyService
     Task<IEnumerable<AgencyDTO>> GetAllAgenciesAsync();
     
     Task<AgencyDTO?> FindAgencyAsync(int id);
+
+    Task<int?> CreateAgencyAsync(AgencyInputDTO agency);
 }
