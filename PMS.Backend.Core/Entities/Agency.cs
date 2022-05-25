6b5a96905e66b3256f5d7fa7f6fb459220b21ab0
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-05-25 13:52:01 +0200

    Add precision to decimal values

--- PMS.Backend.Core/Entities/Agency.cs
+++ PMS.Backend.Core/Entities/Agency.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 using PMS.Backend.Common.Models;
 
 namespace PMS.Backend.Core.Entities;
@@ -12,8 +13,10 @@ public class Agency
     [MaxLength(255)]
     public string LegalName { get; set; }
 
+    [Precision(18,2)]
     public decimal? DefaultCommissionRate { get; set; }
 
+    [Precision(18, 2)]
     public decimal? DefaultCommissionOnExtras { get; set; }
 
     public CommissionMethod CommissionMethod { get; set; }
