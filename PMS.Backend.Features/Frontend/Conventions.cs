8eab01217c533c9ebdb27ffe6bdc06ccc47e173f
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-05-25 15:20:29 +0200

    Add Get and Find Conventions

---
+++ PMS.Backend.Features/Frontend/Conventions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+// ReSharper disable UnusedParameter.Global
+
+[assembly: ApiConventionType(typeof(DefaultApiConventions))]
+namespace PMS.Backend.Features.Frontend;
+
+public static class Conventions
+{
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public static void GetAllAsync() {}
+    
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public static void FindAsync(int id) {}
+}
