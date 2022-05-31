250a159e2ae33767f520d27ff584f799617f8549
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-05-31 22:48:53 +0200

    Exclude from coverage

--- PMS.Backend.Features/Registrar.cs
+++ PMS.Backend.Features/Registrar.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using PMS.Backend.Features;
 using PMS.Backend.Features.Frontend.Agency;
@@ -7,6 +8,7 @@ using PMS.Backend.Features.Frontend.Reservation;
 [assembly: ApiConventionType(typeof(Conventions))]
 namespace PMS.Backend.Features;
 
+[ExcludeFromCodeCoverage]
 public static class Registrar
 {
     public static void AddAPI(this IServiceCollection services)
