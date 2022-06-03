b23c25ccc3b8a55529654e6ca393e96ccb368ec9
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-06-03 14:27:03 +0200

    Add Reservation AutoMapper profile

---
+++ PMS.Backend.Features/Frontend/Reservation/Profile.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using PMS.Backend.Core.Entities.Reservation;
+using PMS.Backend.Features.Frontend.Reservation.Models.Input;
+using PMS.Backend.Features.Frontend.Reservation.Models.Output;
+
+namespace PMS.Backend.Features.Frontend.Reservation;
+
+[ExcludeFromCodeCoverage]
+public class Profile : AutoMapper.Profile
+{
+    public Profile()
+    {
+        CreateMap<GroupReservation, GroupReservationSummaryDTO>();
+        CreateMap<GroupReservation, GroupReservationDetailDTO>();
+        CreateMap<CreateGroupReservationDTO, GroupReservation>();
+        CreateMap<UpdateGroupReservationDTO, GroupReservation>();
+
+        CreateMap<Core.Entities.Reservation.Reservation, ReservationDTO>();
+        CreateMap<CreateReservationDTO, Core.Entities.Reservation.Reservation>();
+        CreateMap<UpdateReservationDTO, Core.Entities.Reservation.Reservation>();
+
+        CreateMap<ReservationDetail, ReservationDetailDTO>();
+        CreateMap<CreateReservationDetailDTO, ReservationDetail>();
+        CreateMap<UpdateReservationDetailDTO, ReservationDetail>();
+    }
+}
