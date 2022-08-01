979819bf0fbeed0a87f039f0852fbb30b625724c
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-08-01 23:37:08 +0200

    Add auto converter between Entities and EDM Model

--- PMS.Backend.Service/Extensions/DbContextExtensions.cs
+++ PMS.Backend.Service/Extensions/DbContextExtensions.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OData.Edm;
 using Microsoft.OData.ModelBuilder;
+using PMS.Backend.Core.Entities;
 using PMS.Backend.Core.Entities.Agency;
 
 namespace PMS.Backend.Service.Extensions;
@@ -8,8 +10,13 @@ namespace PMS.Backend.Service.Extensions;
 /// <summary>
 /// Extensions for <see cref="DbContext"/> instances.
 /// </summary>
+/// TODO: Source Generator
 public static class DbContextExtensions
 {
+    private static MethodInfo ModelBuilderEntitySetMethod { get; } =
+        typeof(ODataConventionModelBuilder)
+            .GetMethod(nameof(ODataConventionModelBuilder.EntitySet))!;
+
     /// <summary>
     /// Generates a <see cref="EdmModel"/> from a given <see cref="DbContext"/>.
     /// </summary>
@@ -21,9 +28,34 @@ public static class DbContextExtensions
         where T : DbContext
     {
         var builder = new ODataConventionModelBuilder();
-        builder.EntitySet<Agency>("Agencies");
-        builder.EntitySet<AgencyContact>("AgencyContacts");
+
+        foreach (var propertyInfo in typeof(T).GetProperties())
+        {
+            if (IsSubclassOfRawGeneric(typeof(DbSet<>), propertyInfo.PropertyType))
+            {
+                var entityType = propertyInfo.PropertyType.GenericTypeArguments[0];
+                ModelBuilderEntitySetMethod
+                    .MakeGenericMethod(entityType)
+                    .Invoke(builder, new[] { propertyInfo.Name as object });
+            }
+        }
 
         return builder.GetEdmModel();
+    }
+
+    private static bool IsSubclassOfRawGeneric(Type generic, Type? toCheck)
+    {
+        while (toCheck != null && toCheck != typeof(object))
+        {
+            var cur = toCheck.IsGenericType ? toCheck.GetGenericTypeDefinition() : toCheck;
+            if (generic == cur)
+            {
+                return true;
+            }
+
+            toCheck = toCheck.BaseType;
+        }
+
+        return false;
     }
 }
