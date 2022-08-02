5d6a4b2c30dc5ddb9fa96ff1ef6e8f8213835570
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-08-02 13:50:59 +0200

    Add global soft delete

---
+++ PMS.Backend.Core/Extensions/ChangeTrackerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using PMS.Backend.Core.Entities;
+
+namespace PMS.Backend.Core.Extensions;
+
+/// <summary>
+/// Extensions for <see cref="ChangeTracker"/>.
+/// </summary>
+public static class ChangeTrackerExtensions
+{
+
+    /// <summary>
+    /// Sets all audit properties on a modified entry and converts hard deleted entries to soft
+    /// deletions.
+    /// </summary>
+    /// <param name="changeTracker">The <see cref="ChangeTracker"/>.</param>
+    public static void SetAuditProperties(this ChangeTracker changeTracker)
+    {
+        changeTracker.DetectChanges();
+        var entries = changeTracker.Entries()
+            .Where(x => x.Entity is Entity && x.State == EntityState.Deleted);
+
+        foreach (var entry in entries)
+        {
+            var entity = (Entity)entry.Entity;
+            entity.IsDeleted = true;
+            entry.State = EntityState.Modified;
+        }
+    }
+}
