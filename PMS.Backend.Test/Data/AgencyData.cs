16752c595fa90d83e62d61f0a12139ff6e58e58d
jorgeparavicini <j.m.paravicini@gmail.com>
2023-04-22 10:38:48 +0200

    Update pipeline and finish create agency contact tests
    * Update pipeline and finish create agency contact tests
    
    * specify solution when restoring
    
    * specify solution when building
    
    * specify solution when testing
    
    * Add codecov.yml
    
    * Fix code inspections

---
+++ PMS.Backend.Test/Data/AgencyData.cs
@@ -0,0 +1,50 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AgencyData.cs" company="Vira Vira">
+// Copyright (c) Vira Vira. All rights reserved.
+// Licensed under the Vira Vira Proprietary License license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using AutoFixture;
+using AutoFixture.Dsl;
+using AutoFixture.Kernel;
+using PMS.Backend.Core.Entities.Agency;
+using PMS.Backend.Core.Entities.Reservation;
+using PMS.Backend.Test.Common.Customization;
+
+namespace PMS.Backend.Test.Data;
+
+public static class AgencyData
+{
+    private static readonly Fixture Fixture;
+
+    static AgencyData()
+    {
+        Fixture = new Fixture();
+        Fixture.Customize(new CompositeCustomization(
+            new EntityCustomization(),
+            new CommissionCustomization<Agency>(
+                agency => agency.DefaultCommissionRate,
+                agency => agency.DefaultCommissionOnExtras)));
+        Fixture.Customize<Agency>(
+            composer => composer.WithoutAggregateRelations(Fixture));
+        Fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+    }
+
+    public static IEnumerable<Agency> GetMultiple(int count = 10) => Fixture.CreateMany<Agency>(count);
+
+    private static IPostprocessComposer<Agency> WithoutAggregateRelations(
+        this IPostprocessComposer<Agency> composer,
+        ISpecimenBuilder fixture)
+    {
+        return composer.With(
+            agency => agency.AgencyContacts,
+            () =>
+            {
+                var agencyContacts = fixture.Create<List<AgencyContact>>();
+                agencyContacts.ForEach(agencyContact => agencyContact.GroupReservations = new List<GroupReservation>());
+                return agencyContacts;
+            });
+    }
+}
--- PMS.Backend.Test/Unit/Features/Agency/Mutations/CreateAgencyContactMutationFixture.cs
+++ PMS.Backend.Test/Unit/Features/Agency/Mutations/CreateAgencyContactMutationFixture.cs
@@ -6,45 +6,34 @@
 // -----------------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoFixture;
 using AutoMapper;
-using Detached.Mappers.EntityFramework;
 using FluentAssertions;
 using HotChocolate;
 using Microsoft.EntityFrameworkCore;
-using PMS.Backend.Core.Database;
-using PMS.Backend.Core.Entities.Agency;
 using PMS.Backend.Features.Features.Agency;
 using PMS.Backend.Features.Features.Agency.Extensions;
 using PMS.Backend.Features.Features.Agency.Models.Input;
 using PMS.Backend.Features.Features.Agency.Models.Payload;
 using PMS.Backend.Features.Features.Agency.Mutations;
 using PMS.Backend.Test.Common.Logging;
+using PMS.Backend.Test.Common.SqlServer;
 using Xunit;
 using Xunit.Categories;
 
 namespace PMS.Backend.Test.Unit.Features.Agency.Mutations;
 
 [UnitTest]
-public class CreateAgencyContactMutationFixture
+public class CreateAgencyContactMutationFixture : TestDatabaseFixture
 {
-    private readonly PmsDbContext _dbContext;
     private readonly IMapper _mapper;
     private readonly RecordingLogger<CreateAgencyContactMutation> _logger = new();
     private readonly CreateAgencyContactMutation _sut = new();
 
     public CreateAgencyContactMutationFixture()
     {
-        // DbContext
-        DbContextOptions<PmsDbContext> options = new DbContextOptionsBuilder<PmsDbContext>()
-            .UseInMemoryDatabase(databaseName: "CreateAgencyContactMutationFixture")
-            .UseDetached()
-            .Options;
-        _dbContext = new PmsDbContext(options);
-
         // Mapper
         MapperConfiguration mapperConfig = new(cfg => cfg.AddProfile<AgencyProfile>());
         _mapper = mapperConfig.CreateMapper();
@@ -55,12 +44,8 @@ public class CreateAgencyContactMutationFixture
     {
         // Arrange
         Fixture fixture = new();
-        Core.Entities.Agency.Agency agency = fixture.Build<Core.Entities.Agency.Agency>()
-            .With(agency => agency.AgencyContacts, () => new List<AgencyContact>())
-            .Create();
-
-        await _dbContext.Agencies.AddAsync(agency);
-        await _dbContext.SaveChangesAsync();
+        int currentCount = DbContext.AgencyContacts.Count();
+        Core.Entities.Agency.Agency agency = DbContext.Agencies.First();
 
         CreateAgencyContactInput input = fixture.Build<CreateAgencyContactInput>()
             .With(input => input.AgencyId, agency.Id)
@@ -68,11 +53,12 @@ public class CreateAgencyContactMutationFixture
 
         // Act
         IQueryable<AgencyContactPayload> result =
-            await _sut.CreateAgencyContactAsync(_dbContext, input, _mapper, _logger);
+            await _sut.CreateAgencyContactAsync(DbContext, input, _mapper, _logger);
 
         // Assert
         int? entityId = await result.Select(contact => contact.Id).SingleOrDefaultAsync();
-        Assert.NotNull(entityId);
+        entityId.Should().NotBeNull();
+        DbContext.AgencyContacts.Should().HaveCount(currentCount + 1);
 
         _logger.ShouldHaveLogged(() => Backend.Features.Extensions.LoggerExtensions.ExecutingMutation);
         _logger.ShouldHaveLogged(() => LoggerExtensions.AgencyContactCreated);
@@ -89,7 +75,7 @@ public class CreateAgencyContactMutationFixture
 
         // Act
         Func<Task<IQueryable<AgencyContactPayload>>> act = async () =>
-            await _sut.CreateAgencyContactAsync(_dbContext, input, _mapper, _logger);
+            await _sut.CreateAgencyContactAsync(DbContext, input, _mapper, _logger);
 
         // Assert
         await act.Should()
