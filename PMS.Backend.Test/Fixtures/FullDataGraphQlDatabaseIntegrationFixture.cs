26bb9cb673fb19a2ba338a821b37c8b4630e0c31
Jorge Paravicini <j.m.paravicini@gmail.com>
2023-04-24 12:46:56 +0200

    add integration tests for queries

---
+++ PMS.Backend.Test/Fixtures/FullDataGraphQlDatabaseIntegrationFixture.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="FullDataGraphQlDatabaseIntegrationFixture.cs" company="Vira Vira">
+// Copyright (c) Vira Vira. All rights reserved.
+// Licensed under the Vira Vira Proprietary License license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using PMS.Backend.Test.Data;
+
+namespace PMS.Backend.Test.Fixtures;
+
+public class FullDataGraphQlDatabaseIntegrationFixture : GraphQlDatabaseIntegrationFixture
+{
+    protected override async Task SeedDatabase()
+    {
+        DbContext.Agencies.AddRange(AgencyData.DeterministicAgencies);
+        await DbContext.SaveChangesAsync();
+    }
+}
